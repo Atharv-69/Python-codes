cd3fd79a21bdf30b6f573c9951a48bdef90c4702
Atharv-69 <patelatharv30@gmail.com>
2025-02-16 23:16:09 +0530

    Create python1.py

---
+++ python1.py
@@ -0,0 +1,39 @@
+#  Print statement
+print("Hello, World!")
+print(3)
+
+# Comments and escape sequences 
+# This is a comment
+print("Hello I am attending a very great course on \"Machine Learning\"")
+print("Hello I am attending a very great course on \n Machine Learning")
+
+
+#Variables 
+x = 10
+print(x)
+x = "Coder"
+print(x)
+
+# Data types 
+
+#Integers 
+x = 10
+y = -5
+print(type(x))
+print(type(y))
+
+# Float 
+pi = 3.14159
+print(type(pi))
+
+#String 
+name = "John"
+print(name)
+greeting = 'Hello, ' + name  # Concatenation
+print(greeting)
+
+# Typecasting in Python 
+a = "55"
+b = 6
+print(type(a))
+print(a+b)
