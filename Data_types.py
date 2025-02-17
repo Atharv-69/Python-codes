00b5b214a67b685b18498d06fbf42a44ad9002fc
Atharv Patel <158567755+Atharv-69@users.noreply.github.com>
2025-02-17 21:42:18 +0530

    Add files via upload

---
+++ Data_types.py
@@ -0,0 +1,65 @@
+# # Data types 
+
+# #Integers - Whole Numbers
+# x = 10
+# print(x)
+# print(type(x))
+# y = -5
+# print(type(y))
+
+# # Float - Decimal numbers
+# pi = 3.14159
+# print(type(pi))
+
+# # #String - A sequence of characters
+# name = "John"
+# print(name)
+# print(type(name))
+# greeting = "Hello, " + name  # Concatenation
+# print(greeting)
+
+# # Typecasting 
+a = "55"
+b = 6
+# print(a+b)
+# print(type(a))
+
+# print(int(a)+b)
+
+# print(a+b)
+# print(int(a)+b)
+
+# print(a+str(b))
+# print(a+" "+str(b))
+
+# name="Atharv"
+# print(int(name))
+
+
+# #Booleans - Represent True or False values.
+# is_raining = True
+# is_sunny = False
+# print(type(is_raining))
+# print(type(is_sunny))
+# print(is_raining)
+# print(is_sunny) 
+
+
+# #Lists: Ordered collection of items, which can be of any data type. mutable
+# fruits = ["apple", "banana", "cherry"]
+# print(type(fruits))
+# print(fruits)
+
+
+# # Tuples: Similar to lists, but immutable (can't be changed).
+# coordinates = (3, 5)  # Tuple of two integers
+# print(type(coordinates))
+# print(coordinates)
+
+
+# # Dictionaries: Key-value pairs, often used for data mapping.
+# student = {"name": "Alice", "age": 22, "is_active": True}
+# print(type(student))
+# print(student)
+
+
