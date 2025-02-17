5c0f9218f612affc0487fad7017279527a6ad997
Atharv Patel <158567755+Atharv-69@users.noreply.github.com>
2025-02-17 21:43:00 +0530

    Add files via upload

---
+++ print&escape.py
@@ -0,0 +1,14 @@
+#  Print statement
+# print("Hello, World!")
+# x=10 #x is velocity of car
+# print(3)
+
+# Comments and escape sequences 
+# This is a comment
+# print("Hello I am attending a very great course on \"Machine Learning\"")
+# print("Hello I am attending a very great course on \nMachine Learning")
+
+
+# Taking input in python
+x = input("enter a number")
+print(x)
