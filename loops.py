43abf0317b16de33f2e61e13ab438fa127932f6c
Atharv Patel <158567755+Atharv-69@users.noreply.github.com>
2025-02-19 16:48:30 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,36 @@
+# For loops - Iterate on a list
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+
+# iteration on a string
+# for char in "Python":
+#     print(char)
+
+# for i in range(1,10,2):
+#     print (i)
+
+# Make a list of few colors and iterate a for loop on
+#  that list and print all colours
+
+
+#while loops 
+# Example: Print numbers from 1 to 5
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1  
+
+
+# Match - case
+# number = int(input("Enter a number: "))
+
+# match (number):
+#     case 1:
+#         print("Your number is one ")
+#     case 2:
+#         print("Your number is two ")
+#     case 3:
+#         print("Your number is three ")
+
+
