b1494514dd3a8581ebb94903b095287a6e0bb8f7
Atharv Patel <158567755+Atharv-69@users.noreply.github.com>
2025-02-19 16:48:03 +0530

    Add files via upload

---
+++ bitwise_operators.py
@@ -0,0 +1,24 @@
+#bitwise operators 
+
+#1- and operator (&)
+# print(5 & 3) # 0101 & 0011 = 0001 (1)
+
+
+#2- or operator (|)
+# print(5 | 3) # 0101 | 0011 = 0111 (7)
+
+
+#3- xor operator (^)
+# print(5 ^ 3)  # 0101 ^ 0011 = 0110 (6)
+
+
+#4- not operator (~)
+# print(~ 5)  # ~0101  = 1010  (10)
+
+
+#5- left shift (<<)
+# print(5 << 1)   #0101 << 1 = 1010  (10)
+
+
+#6-  right shift (>>)
+# print (5 >> 1)  #0101 >> 1 = 0010 (2)
