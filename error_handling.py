ff1eb041ebf46bbfa37643f63ef114bba8de2519
Atharv Patel <158567755+Atharv-69@users.noreply.github.com>
2025-02-23 16:47:21 +0530

    Add files via upload

---
+++ error_handling.py
@@ -0,0 +1,14 @@
+# a = input("Enter a number:")
+
+# for i in range(1,11):
+#     print(f"{int(a)} X {i} = {int(a)*i}")
+    
+# # try:
+# #     for i in range(1,11):
+# #         print(f"{int(a)} X {i} = {int(a)*i}")
+# # except Exception as e:
+# #     print (e)
+
+
+# print("hello everyone")
+# print("End of program")
